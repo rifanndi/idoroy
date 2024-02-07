0d31d5784ce1d113b3f330a1a473af85c7e06e1f
rifanndi <rifandiannass@gmail.com>
2024-02-07 19:34:00 +0700

    first commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import "../src/index.css";
+import "./index.css";
 function App() {
   return (
     <>
