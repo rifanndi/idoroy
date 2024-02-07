199f6655e8fd7d61e3248d445a3b0b9aec4517d3
rifanndi <rifandiannass@gmail.com>
2024-02-07 20:46:12 +0700

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import About from "./About";
 import Services from "./Services";
-
+import MyCustomCarousel from "./MyCarousel";
 //!import Contact from "./Contact";
 
 function Main() {
   return (
     <>
       <main>
+        <MyCustomCarousel />
         <Services />
 
         <About />
