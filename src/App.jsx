0206d37e4203b9f2387a54191124fcd30fc4375b
rifanndi <rifandiannass@gmail.com>
2024-02-10 07:26:37 +0700

    firsttt

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import MyCustomCarousell from "./components/produkgrid";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Navbar />
       <Header />
       <Main />
-      <MyCustomCarousell />
       <Footer />
     </>
   );
